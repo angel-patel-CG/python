3645145199dcfc9fa3bb944ceffe47b8bc9a646f
angel-patel-CG <angelpatel.cg@gmail.com>
2026-08-24 11:11:48 +0530

    types

---
+++ 1.variables/2_variable_types.py
@@ -0,0 +1,20 @@
+from math import integer
+
+
+print("Hello, World1!")
+print("Hello, World2!")
+print("Hello, World3!")
+print("Hello, World4!")
+
+age=17
+Age=17.5
+aGe="17"
+AgE=True
+Age=None
+
+print("student information:", age, Age, aGe, AgE, Age)
+print(type(age))
+print(type(Age))
+print(type(aGe))
+print(type(AgE))
+print(type(Age))
