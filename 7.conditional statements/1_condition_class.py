4a69d2a3067e3842c618315f66fdf4ad04ed8411
angel-patel-CG <angelpatel.cg@gmail.com>
2026-09-08 09:28:32 +0530

    condition statement

--- 7.conditional statements/1_condition_class.py
+++ 7.conditional statements/1_condition_class.py
@@ -36,12 +36,46 @@
 
 # #create a bank account with a minimum balance of 1000 and check if the user can withdraw money or not
 
-balance = 1000
-withdrawal_amount = float(input("Enter the amount you want to withdraw: "))
+# balance = 1000
+# withdrawal_amount = float(input("Enter the amount you want to withdraw: "))
 
-if withdrawal_amount <= balance:
-    print("Withdrawal successful.")
-    balance -= withdrawal_amount
-    print(f"Remaining balance: ${balance}")
+# if withdrawal_amount <= balance:
+#     print("Withdrawal successful.")
+#     balance -= withdrawal_amount
+#     print(f"Remaining balance: ${balance}")
+# else:
+#     print("Insufficient funds.")
+
+# #elif learning 
+
+# marks = int(input("enter marks:"))
+
+# if marks>=90 :
+#     print("A")
+# elif marks >= 60 :
+#     print("B")
+# else:
+#     print("better luck  next time")
+
+
+## calculator
+
+number_1 = int(input("enter number 1 :"))
+number_2 = int(input("enter number 2 :"))
+
+print (" 1.addition  2.substraction  3.multiplication  4.devision  5.floordevision")
+
+operation = input( "what do you want to perform,enter number or name of operation: ").lower().strip()
+
+if operation == "addition" or operation== str(1) :
+    print(f"addition is : {number_1+number_2}")
+elif operation == "substraction" or operation == str(2) :
+    print(f"substraction is : {number_1-number_2}")
+elif operation == "multiplication" or operation == str(3) :
+    print(f"multiplication is : {number_1*number_2}")
+elif operation == "devision " or operation == str(4) :
+    print(f"devision is : {number_1/number_2}")
+elif operation == "fllordevision" or operation == str(5) :
+    print(f"floordevision is : {number_1//number_2}")
 else:
-    print("Insufficient funds.")
+    print("invalid operation")
--- 7.conditional statements/2_condition_ass.py
+++ 7.conditional statements/2_condition_ass.py
@@ -69,3 +69,190 @@
 #     print("D")
 # if marks<=40:
 #     print("F")
+
+# #12
+
+# number = int(input("enter your number: "))
+# if number>0:
+#     print("Positive")
+# elif number<0:
+#     print("negative")
+# elif number==0  :
+#     print("zero") 
+# else :
+#     print("just a number") 
+
+# #13
+
+# print("1_Monday", "2_Tuesday", "3_Wednesday", "4_Thursday", "5_Friday")
+
+# day = input("enter number for a day:").strip().lower()
+
+# if day== "Monday" or day== str(1):
+#     print("Monday")
+# elif day== "Tuesday" or day== str(2):
+#     print("Tuesday")
+# elif day== "Wednesday" or day== str(3):
+#     print("Wednesday")
+# elif day== "Thursday" or day== str(4):
+#     print("Thursday")
+# elif day== "Friday" or day== str(5):
+#     print("Friday")
+# else:
+#     print("other")
+
+# #14
+
+# marks= int(input("enter your marks: ").strip())
+
+# if marks >= 90:
+#     print("Excellent")
+# elif marks>=70 and marks<=89:
+#     print("Good")
+# elif marks>=50 and marks<=69:
+#     print("Pass")
+# elif marks>=35 and marks<=49:
+#     print("Improvement needed")
+# elif marks<=34:
+#     print("Fail")
+# else:
+#     print("marks")
+
+# #15
+
+# number = int(input("enter your number: "))
+
+# if number == 1:
+#     print("1")
+# elif number == 2:
+#     print("2")
+# elif number == 3:
+#     print("3")
+# else:
+#     print("other")
+
+# #16
+
+# age = int(input("enter your age: ").strip())
+
+# if age >= 18:
+#     print("yes")
+#     if age <=60:
+#         print("Between 18 and 60")
+
+# #17
+
+# marks = int(input("enter your marks: ").strip())
+
+# if marks>=40:
+#     print("Passed")
+#     if marks>=75:
+#         print("Good")
+# else:
+#     print("Falied")        
+
+# #18
+
+# number = int(input("enter your number: ").strip())
+
+# if number > 0:
+#     print("Positive")
+#     if number>100:
+#         print("The given Number is greather than 100!!")
+# else:
+#     print("just a number")
+
+# #19
+
+# age = int(input("enter your age: ").strip())
+
+# if age >= 18:
+#     print("yes")
+#     if age <=60:
+#         print("Between 18 and 60")
+
+# #20
+
+# number = int(input("enter your number: ").strip())
+
+# if number!= 0:
+#     print("non-zero")
+#     if number>0:
+#         print("Positive")
+#     elif number<0:
+#         print("Negative")
+
+# #21
+
+# age = int(input("enter your age: ").strip())
+
+# marks = int(input("enter your marks: ").strip())
+
+# if age >= 18 and marks >= 40:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+# #22
+
+# number = int(input("enter your number: ").strip())
+
+# if number < 10 or number > 100:
+#     print("Special")
+
+# #23
+
+# age=int(input("Enter your age:"))
+# has_id=True
+# if age>=18 and has_id==True:
+#     print("Allowed")
+
+##24
+
+# first_number=int(input("Enter first number"))
+# second_number=int(input("Enter second number"))
+# if first_number>10 and second_number>10:
+#     print("Both are greater than 10")
+
+##25
+
+# first_number=int(input("Enter first number"))
+# second_number=int(input("Enter second number"))
+# if first_number<0 or second_number>100:
+#     print("Both are in range")
+
+##27
+
+# number=int(input("Enter number"))
+
+# if number>10 and number<50:
+#     print("The number is between 10-50")
+
+##28
+
+# number=int(input("Enter number"))
+
+# if number>10 or number<50:
+#     print("The number is between 10-50")
+
+##29
+
+# is_student = True
+# has_id = True
+# has_ticket = True
+
+
+# if is_student and has_id and has_ticket:
+#     print("Allowed")
+
+##30
+
+age = int(input("Enter your age: "))
+marks = int(input("Enter your marks: "))
+has_id = input("Do you have an ID? (True/False): ") == "True"
+if age >= 18 and marks >= 40 and has_id is True:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+
